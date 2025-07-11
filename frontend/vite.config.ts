68f688f01a4fa111c8322291b1ac11c718c68ca0
cmraymo1@butler.edu <cmraymo1@butler.edu>
2025-07-11 09:37:43 -0500

    changed to work with server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 80,
+    allowedHosts: ['www.chrisraymond.dev'],
     ...(mode === 'development' && {
       proxy: {
         '/api': {
